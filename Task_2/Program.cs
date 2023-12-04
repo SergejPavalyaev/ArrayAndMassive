f7eadad7aa86bc5ac7ef9ecfa1aa003c137627c0
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-12-04 23:02:14 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_2
+{
+    public class Program
+    {
+        public static void Main (string [] args){
+            // Задайте строку, содержащую латинские буквы
+            // в обоих регистрах. Сформируйте строку, в которой все
+            // заглавные буквы заменены на строчные.
+            System.Console.Write("Input string =>");
+            string ? str = System.Console.ReadLine();
+            string newStr = str.ToLower();
+            System.Console.WriteLine($"new string => {newStr}");
+        }
+    }
+}
