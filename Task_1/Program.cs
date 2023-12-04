93ef0a6caaf43d6891087f9cce5a597e71400305
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-12-04 22:25:05 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_1
+{
+    public class Program
+    {
+        public static void Main (string [] args){
+        // Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива. 
+            int size = 3;
+            char [,] chars = new char [size];
+        }
+    }
+}
