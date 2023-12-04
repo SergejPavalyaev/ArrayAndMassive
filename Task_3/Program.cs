f2d1cbf41c1fb7a742fc25670f2b7544ae908a32
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-12-04 23:13:11 +0300

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_3
+{
+    public class Program
+    {
+        public static void Main (string [] args){
+            // Задайте произвольную строку. Выясните, является ли она палиндромом.
+            System.Console.Clear();
+            System.Console.Write("Input string => ");
+            string ? str = System.Console.ReadLine();
+            bool flag = CheckPalindrome(str);
+            if (flag) System.Console.WriteLine("Yes");
+            else System.Console.WriteLine("No");
+        }
+        public static bool CheckPalindrome (string str){
+            int size = str.Length;
+            bool flag = false;
+            for (int i = 0; i < size / 2; i++)
+            {
+                if (str[i] == str[size-1-i]) flag = true;
+                else flag = false;
+            }
+            return flag;
+        }
+    }
+}
